edcb015046a4190c2b2fe944ba938f84058d7898
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-09-08 15:56:46 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,6 +17,11 @@ shipping_manifest["oil paintings"]
 end
 
 def adding
+  shipping_manifest = {
+  "whale bone corsets" => 5,
+  "oil paintings" => 3,
+  "porcelain vases" => 2
+    }
 shipping_manifest["muskets"] = 2
 shipping_manifest["gun powder"] = 4
 shipping_manifest
