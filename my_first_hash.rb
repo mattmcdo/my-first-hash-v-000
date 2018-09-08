c329f419be9bbd9aa6e2a30e1a60b978e0268efd
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-09-08 15:47:22 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,3 +3,11 @@ my_hash = {
 "my_name" => "Matt"
 }
 end
+
+def shipping_manifest
+  items = {
+  "whale" = "whale bone corsets", 
+  "art" = "oil paintings", 
+  "decor" = "porcelain vases"
+    }
+  end
