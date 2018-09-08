f5daaa9fbe2134ab2092d13c61bdeb94ff110002
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-09-08 15:57:27 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,40 +1,28 @@
 def my_hash
-  my_hash = {"name" => "Matt"}
+my_hash = {
+"my_name" => "Matt"
+}
 end
-
 
 def shipping_manifest
-    the_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
+  shipping_manifest = {
+  "whale bone corsets" => 5,
+  "oil paintings" => 3,
+  "porcelain vases" => 2
     }
-end
+  end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-
 shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
+  "whale bone corsets" => 5,
+  "oil paintings" => 3,
+  "porcelain vases" => 2
+    }
 shipping_manifest["muskets"] = 2
-
-
-  # add 4 gun powder to the shipping_manifest hash below
 shipping_manifest["gun powder"] = 4
-
-
-  # return the shipping_manifest hash below
-return shipping_manifest
+shipping_manifest
 end
